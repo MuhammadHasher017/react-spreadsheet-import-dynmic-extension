4128af8ca66df7d2204f2e5c833bc82502561db6
MH08 <muhammadhasher017@gmail.com>
2024-09-24 18:23:48 +0500

    first commit

---
+++ src/steps/UploadStep/utils/acceptedTypes.ts
@@ -0,0 +1,24 @@
+type AcceptedTypes = {
+  [key: string]: string[]
+}
+
+const acceptedTypes: AcceptedTypes = {
+  "application/vnd.ms-excel": [".xls"],
+  "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet": [".xlsx"],
+  "text/csv": [".csv"],
+}
+
+export function getAcceptedTypes(inputExtensions: string[]): AcceptedTypes {
+  const result: AcceptedTypes = {}
+
+  inputExtensions.forEach((extension) => {
+    for (const [mimeType, extensions] of Object.entries(acceptedTypes)) {
+      if (extensions.includes(extension)) {
+        result[mimeType] = extensions
+      }
+    }
+  })
+
+  console.log("object", result)
+  return result
+}
